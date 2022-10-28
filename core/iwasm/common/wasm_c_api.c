86ed091365292c638ca71ab44260e3a40032c2b7
Wenyong Huang <wenyong.huang@intel.com>
2022-10-28 16:58:01 +0800

    implement win static mutex, refine code

--- core/iwasm/common/wasm_c_api.c
+++ core/iwasm/common/wasm_c_api.c
@@ -328,23 +328,22 @@ wasm_engine_new_internal(mem_alloc_type_t type, const MemAllocOption *opts)
 /* global engine instance */
 static wasm_engine_t *singleton_engine = NULL;
 #if defined(OS_THREAD_MUTEX_INITIALIZER)
+/**
+ * lock for the singleton_engine
+ * Note: if the platform has mutex initializer, we use a global lock to
+ * lock the operations of the singleton_engine, otherwise when there are
+ * operations happening simultaneously in multiple threads, developer
+ * must create the lock by himself, and use it to lock the operations
+ */
 static korp_mutex engine_lock = OS_THREAD_MUTEX_INITIALIZER;
-#else
-static korp_mutex engine_lock;
 #endif
 
 own wasm_engine_t *
 wasm_engine_new_with_args(mem_alloc_type_t type, const MemAllocOption *opts)
 {
-#if !defined(OS_THREAD_MUTEX_INITIALIZER)
-    if (!singleton_engine) {
-        if (os_mutex_init(&engine_lock) != 0) {
-            return NULL;
-        }
-    }
-#endif
-
+#if defined(OS_THREAD_MUTEX_INITIALIZER)
     os_mutex_lock(&engine_lock);
+#endif
 
     if (!singleton_engine) {
         singleton_engine = wasm_engine_new_internal(type, opts);
@@ -354,13 +353,8 @@ wasm_engine_new_with_args(mem_alloc_type_t type, const MemAllocOption *opts)
         singleton_engine->ref_count++;
     }
 
+#if defined(OS_THREAD_MUTEX_INITIALIZER)
     os_mutex_unlock(&engine_lock);
-
-#if !defined(OS_THREAD_MUTEX_INITIALIZER)
-    if (!singleton_engine) {
-        /* failed to new engine */
-        os_mutex_destroy(&engine_lock);
-    }
 #endif
 
     return singleton_engine;
@@ -382,9 +376,16 @@ wasm_engine_new_with_config(own wasm_config_t *config)
 void
 wasm_engine_delete(wasm_engine_t *engine)
 {
-    bh_assert(singleton_engine && engine == singleton_engine);
-
+#if defined(OS_THREAD_MUTEX_INITIALIZER)
     os_mutex_lock(&engine_lock);
+#endif
+
+    if(!engine || !singleton_engine || engine != singleton_engine) {
+#if defined(OS_THREAD_MUTEX_INITIALIZER)
+        os_mutex_unlock(&engine_lock);
+#endif
+        return;
+    }
 
     if (singleton_engine->ref_count > 0) {
         singleton_engine->ref_count--;
@@ -394,12 +395,8 @@ wasm_engine_delete(wasm_engine_t *engine)
         }
     }
 
+#if defined(OS_THREAD_MUTEX_INITIALIZER)
     os_mutex_unlock(&engine_lock);
-
-#if !defined(OS_THREAD_MUTEX_INITIALIZER)
-    if (!singleton_engine) {
-        os_mutex_destroy(&engine_lock);
-    }
 #endif
 }
 
