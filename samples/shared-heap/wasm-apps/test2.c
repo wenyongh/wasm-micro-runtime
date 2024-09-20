d5745750134189dd7f33922b8ccc7ca1a8fbf3b7
Wenyong Huang <wenyong.huang@intel.com>
2024-09-20 14:46:08 +0800

    add license header

--- samples/shared-heap/wasm-apps/test2.c
+++ samples/shared-heap/wasm-apps/test2.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2019 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#include <stdio.h>
+
 #include <stdio.h>
 
 extern void
